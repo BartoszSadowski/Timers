0bfe7f9bf225fd3765c9381b4c77b8d56930510e
Sadaffi <bartek_sadowski@o2.pl>
2021-03-15 17:06:08 +0100

    [basic-layout] Added AppBar

---
+++ src/components/common/AppBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <v-app-bar dense>
+      <v-toolbar-title>Timers app</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppBar',
+};
+</script>
