10c3d6147310bb80c8aba72729c8f2fa7f0d677b
Sadaffi <bartek_sadowski@o2.pl>
2021-03-15 17:06:42 +0100

    [basic-layout] Used layout components for App view

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <v-app>
-    Hello World!
+    <AppBar />
+    <Main />
   </v-app>
 </template>
 
 <script>
+import Main from './components/views/Main.vue';
+import AppBar from './components/common/AppBar.vue';
+
 export default {
   name: 'App',
+  components: {
+    Main,
+    AppBar,
+  },
 };
 </script>
