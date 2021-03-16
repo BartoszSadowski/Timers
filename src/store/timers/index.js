d84dfb1cf947c51926ee3b298dc14586d5045995
Sadaffi <bartek_sadowski@o2.pl>
2021-03-16 21:52:21 +0100

    [timer-control] Removed placeholder timers

--- src/store/timers/index.js
+++ src/store/timers/index.js
@@ -2,13 +2,7 @@ import actions from './actions';
 import mutations from './mutations';
 
 const state = {
-  list: [{
-    id: 1,
-    date: new Date(2022, 4, 16, 12, 12, 12),
-  }, {
-    id: 2,
-    date: new Date(2021, 8, 14, 13, 13, 13),
-  }],
+  list: [],
 };
 
 export default {
