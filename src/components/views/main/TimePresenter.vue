6da4f804ca39871b249c16bd0945eb767d0939e4
Sadaffi <bartek_sadowski@o2.pl>
2021-03-16 19:50:28 +0100

    [timer-item] Added component for presenting countdown

---
+++ src/components/views/main/TimePresenter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="ma-2 pa-2">
+    <v-row>
+      {{ title }}
+    </v-row>
+    <v-row>
+      {{ number }}
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TimePresenter',
+
+  props: {
+    number: {
+      type: Number,
+      required: true,
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
--- src/components/views/main/TimerItem.vue
+++ src/components/views/main/TimerItem.vue
@@ -1,18 +1,45 @@
 <template>
   <v-sheet class="pa-2 ma-2" elevation="1">
-    Left
-      {{ timeLeft.days }}
-      : {{ timeLeft.hours }}
-      : {{ timeLeft.minutes }}
-      : {{ timeLeft.seconds }}
+      <v-row>
+        <v-col lg="3">
+          <TimePresenter
+            :number="timeLeft.days"
+            title="Days"
+          />
+        </v-col>
+        <v-col lg="3">
+          <TimePresenter
+            :number="timeLeft.hours"
+            title="Hours"
+          />
+        </v-col>
+        <v-col lg="3">
+          <TimePresenter
+            :number="timeLeft.minutes"
+            title="Minutes"
+          />
+        </v-col>
+        <v-col lg="3">
+          <TimePresenter
+            :number="timeLeft.seconds"
+            title="Seconds"
+          />
+        </v-col>
+      </v-row>
   </v-sheet>
 </template>
 
 <script>
 import { msToTimeObj } from '../../../utils/time';
 
+import TimePresenter from './TimePresenter.vue';
+
 export default {
   name: 'TimerItem',
+
+  components: {
+    TimePresenter,
+  },
 
   props: {
     timer: {
