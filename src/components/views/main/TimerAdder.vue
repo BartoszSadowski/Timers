06977d0bbceb5b392af2285557355ae90f59633c
Sadaffi <bartek_sadowski@o2.pl>
2021-03-16 20:28:56 +0100

    [timer-control] Added control layout

--- src/components/views/main/TimerAdder.vue
+++ src/components/views/main/TimerAdder.vue
@@ -7,7 +7,22 @@
       Add new timer
     </v-card-title>
 
-    <v-card-text> Placeholder text </v-card-text>
+    <form>
+      <v-row class="pl-4">
+        <v-col md="10">
+          Date placeholder
+        </v-col>
+        <v-col>
+          <v-btn
+                icon
+                elevation="2"
+                @click="removeItem(timer)"
+              >
+                <v-icon>mdi-plus</v-icon>
+              </v-btn>
+        </v-col>
+      </v-row>
+    </form>
   </v-card>
 </template>
 
