19da5fb4d7ad96579a0ea9f80ea1b4590f1b7d53
Sadaffi <bartek_sadowski@o2.pl>
2021-03-17 19:24:01 +0100

    [internationalization] Added menu for choosing language

--- src/components/common/AppBar.vue
+++ src/components/common/AppBar.vue
@@ -6,6 +6,40 @@
         :label="darkModeMessage"
         class="mt-5"
       />
+
+      <v-spacer />
+
+      <v-menu
+        bottom
+        left
+        offset-y
+      >
+        <template v-slot:activator="{ on, attrs}">
+          <v-btn
+            icon
+            class="mr-1"
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="locale in locales"
+            :key="locale"
+            class="pa-0"
+          >
+            <v-btn
+              text
+              class="ma-0"
+            >
+              {{ locale }}
+            </v-btn>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
   </div>
 </template>
@@ -14,9 +48,15 @@
 export default {
   name: 'AppBar',
 
+  data() {
+    return {
+      locales: process.env.VUE_APP_I18N_SUPPORTED_LOCALE.split(','),
+    };
+  },
+
   computed: {
     darkModeMessage() {
-      return this.$vuetify.theme.dark ? 'Switch to light mode' : 'Switch to dark mode';
+      return this.$vuetify.theme.dark ? this.$t('THEME.SWITCH_LIGHT') : this.$t('THEME.SWITCH_DARK');
     },
   },
 };
