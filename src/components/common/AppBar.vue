5fb504374ae6b487c1dd5f337bb5a03129767ec3
Sadaffi <bartek_sadowski@o2.pl>
2021-03-17 18:50:07 +0100

    [dark-mode] Added dark mode switch

--- src/components/common/AppBar.vue
+++ src/components/common/AppBar.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <v-app-bar dense app>
-      <v-toolbar-title>Timers app</v-toolbar-title>
+      <v-switch
+        v-model="$vuetify.theme.dark"
+        :label="darkModeMessage"
+        class="mt-5"
+      />
     </v-app-bar>
   </div>
 </template>
@@ -9,5 +13,11 @@
 <script>
 export default {
   name: 'AppBar',
+
+  computed: {
+    darkModeMessage() {
+      return this.$vuetify.theme.dark ? 'Switch to light mode' : 'Switch to dark mode';
+    },
+  },
 };
 </script>
